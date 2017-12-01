62c5f745e2927d21c81c4db46014d642cec2e03d
xc <410014964@qq.com>
2017-12-01 11:18:51 +0800

    测试git

---
+++ src/main/java/com/xc/rocketmq/git.java
@@ -0,0 +1,10 @@
+package com.xc.rocketmq;
+
+/**
+ * Created by xc on 2017/12/1.
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
